6f5f5317e866d1b609342a5f195a23a71e58dd90
HEPL-Starygin <124890653+HEPL-Starygin@users.noreply.github.com>
2023-05-01 15:27:00 +0200

    Add files via upload

---
+++ Network/secrets.py
@@ -0,0 +1,10 @@
+# Fill the empty string with the relevant information
+my_secrets = {
+    "ssid" : "NOM",              #remplacer NOM par le nom du wifi
+    "WiFi_pass" : "mdp",         #remplacer mdp par le mot-de-passe du wifi
+    "OWM_API_key" : "KEY",       #remplacer KEY par votre clé à l'API du site
+    # My location :
+    "lat" : "LAT",               #remplacer LAT par la latitude que vous voulez
+    "lon" : "LON",                  #remplacer LON par la longitude que vous voulez
+    "units" : "metric"
+}
